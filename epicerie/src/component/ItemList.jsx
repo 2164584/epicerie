6b7229e64964bfd20eb51f5d4fa9da0f18b75716
Gabriel <2164584@claurendeau.qc.ca>
2024-02-20 17:54:11 -0500

    debut site

---
+++ epicerie/src/component/ItemList.jsx
@@ -0,0 +1,13 @@
+function ItemList() {
+  return (
+    <div className='row'>
+      <div className="col-12">
+        <h3>Items</h3>
+        <ul>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ItemList;
